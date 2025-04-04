eb9d3391ec74e982a72a74b538660e30e97f8ccb
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-04 13:04:44 -0600

    Frontend

---
+++ PasteleriaProyecto/src/main/java/Admin_Pasteleria/Controller/NavegationController.java
@@ -0,0 +1,29 @@
+package Admin_Pasteleria.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NavegationController {
+
+        @GetMapping("/") // Página principal
+    public String mostrarIndex() {
+        return "index"; // Esto buscará index.html en templates/
+    }
+
+    @GetMapping("/contacto") // Nueva ruta para vista.html
+    public String mostrarContacto() {
+        return "contacto"; // Esto buscará vista.html en templates/
+    }
+    
+    @GetMapping("/menu") // Nueva ruta para vista.html
+    public String mostrarMenu() {
+        return "menu"; // Esto buscará vista.html en templates/
+    }
+    
+    @GetMapping("/trabajos") // Nueva ruta para vista.html
+    public String mostrarTrabajos() {
+        return "trabajos"; // Esto buscará vista.html en templates/
+    }
+
+}
---
+++ PasteleriaProyecto/src/test/java/Admin_Pasteleria/Pasteleria/PasteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package Admin_Pasteleria.Pasteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
