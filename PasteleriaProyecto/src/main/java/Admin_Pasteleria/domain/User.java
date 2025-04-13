c3db5c406b7d0d25010af355b62617cd6be8e8a6
Sebastián Roberto Méndez Artavia <sebasmendeza09@gmail.com>
2025-04-12 23:28:59 -0600

    User,java
    User.java

---
+++ PasteleriaProyecto/src/main/java/Admin_Pasteleria/domain/User.java
@@ -0,0 +1,43 @@
+package Admin_Pasteleria.domain;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+@Data
+@Table(name="usuario")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+
+    @OneToMany
+    @JoinColumn(name="id_usuario")
+    private List<Rol> roles;
+}
